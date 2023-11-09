941f70ad2ebb32361a99f0b74a6cfc9a3321b5c1
CamiloCastrillon <juan.navia.castrillon@correounivalle.edu.co>
2023-11-09 16:22:19 -0500

    avance visual 2

---
+++ js/map.js
@@ -0,0 +1,13 @@
+var map = L.map('map', {
+    maxBounds: L.latLngBounds(
+        L.latLng(4.58, -76.20), // Esquina superior izquierda del área permitida
+        L.latLng(4.5, -76.0)  // Esquina inferior derecha del área permitida
+      )
+  }).setView([4.54, -76.10], 12);
+
+var mapa_base = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+    minZoom: 12,
+	maxZoom: 16
+}).addTo(map);
